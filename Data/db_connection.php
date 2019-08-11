ccb5371f19c3538eb41acc522c5f9058786a99d9
Adory Vo <adoryvo@gmail.com>
2019-08-10 18:08:39 -0700

    Added Help Page, Lab Recommendations Page, Settings, and Seminars Page

--- Data/db_connection.php
+++ Data/db_connection.php
@@ -1,23 +1,21 @@
 <?php
-
 //Make sure to INCLUDE this file when accessing mysql databases
 
-//Creatitng an array for database connection
+//Creating an array for database connection
 $db["db_host"] = "localhost";
 $db["db_username"] = "root";
 $db["db_pass"] = "";
 $db["db_name"] = "cms";
 
 //Converting array values into constants --> more security
-foreach($db as $key => $value){
-  //Setting up constants
-  define(strtoupper($key),$value);
+foreach ($db as $key => $value) {
+    //Setting up constants
+    define(strtoupper($key), $value);
 }
 //Setting up php connection
-$connection = mysqli_connect(DB_HOST,DB_USERNAME,DB_PASS,DB_NAME);
+$connection = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASS, DB_NAME);
 
-if(!$connection){
-  die("Connection failed <br>" . mysql_error());
+if (!$connection) {
+    die("Connection failed <br>" . mysql_error());
 }
-
 ?>
