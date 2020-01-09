491dbcf6dbe9d29e1dc0182b386ca0586294d016
Adory Vo <adoryvo@gmail.com>
2020-01-09 01:13:18 -0800

    remove php functionalities & organize tree

---
+++ php/scripts/user-profile-update.php
@@ -0,0 +1,23 @@
+<?php include 'session-variables-js.php'; ?>
+
+<script>
+
+//DOM SELECTION
+let profile_card = document.querySelector(".profile-card");
+let name = document.querySelector(".name");
+let email_dom = document.querySelector(".email");
+let institution_dom = document.querySelector(".institution");
+let building_dom = document.querySelector(".building");
+let department_dom = document.querySelector(".department");
+let division_dom = document.querySelector(".division");
+
+//Dynamic uploading
+name.textContent = first_name + " " + last_name;
+email_dom.textContent = email;
+
+institution_dom.textContent = institution;
+building_dom.textContent = building;
+department_dom.textContent = department;
+division_dom.textContent = division;
+
+</script>
